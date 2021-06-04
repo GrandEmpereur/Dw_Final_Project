df92b9011d3f2c60e1d3f5bb7cf9dc492d848e69
Grand Empereur | <bartosikpatrick2@gmail.com>
2021-06-04 17:07:09 +0200

    add blog post

---
+++ blogPost.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<? session_start() ?>
+    <?php 
+        include('./php/link.php');
+    ?>
+<body>
+    <?php 
+        include('./php/header.php');
+    ?>
+        
+
+    </main>
+
+    <script src="main.js"></script>
+    </body>
+
+    </html>
